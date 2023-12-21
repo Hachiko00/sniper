f9c4cef7508f8d5d4d20dfe446d2bc38e2cbac51
Hachiko00 <78735962+Hachiko00@users.noreply.github.com>
2023-12-22 07:20:23 +0800

    Update sniperniyen.lua

--- sniperniyen.lua
+++ sniperniyen.lua
@@ -1,15 +1,19 @@
+print('executed')
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 
-if not getgenv().a then
-    getgenv().a = true
-    local vu = game:GetService("VirtualUser")
-    game:GetService("Players").LocalPlayer.Idled:connect(function()
-        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-        wait(1)
-        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-    end)
+for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+	v:Disable()
 end
+-- for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+	-- v:Disable()
+-- end
 
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    wait(1)
+    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
 local function processListingInfo(uid, gems, item, version, shiny, amount, boughtFrom)
     print(uid, gems, item, version, shiny, amount, boughtFrom)
     print("BOUGHT FROM:", boughtFrom)
@@ -43,58 +47,19 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
         amount = 1
         print("AMOUNT:", amount)
     end
-
-    local fields = {
-        {
-            name = "PRICE:",
-            value = tostring(gems) .. " GEMS",
-            inline = true,
-        },
-        {
-            name = "BOUGHT FROM:",
-            value = tostring(boughtFrom),
-            inline = true,
-        },
-        {
-            name = "AMOUNT:",
-            value = tostring(amount),
-            inline = true,
-        },
-        {
-            name = "PETID:",
-            value = tostring(uid),
-            inline = true,
-        }
-    }
-
-    local message = {
-        content = "@everyone",
-        embeds = {
-            {
-                title = snipeMessage,
-                fields = fields,
-                author = {name = "New Pet Sniped!"}
-            }
-        },
-        username = "diakoto",
-        attachments = {}
-    }
-
-    local http = game:GetService("HttpService")
-    local jsonMessage = http:JSONEncode(message)
-
-    http:PostAsync(
-        "https://discord.com/api/webhooks/1186866649492635648/6CYf7FV4SA6p-6QZoI9XCLBYxB_Tu5VN458HA3Rp7eAAHZA-Mbs_PXTMU8e4e_MQqLfm",
-        jsonMessage,
-        Enum.HttpContentType.ApplicationJson,
-        false
-    )
 end
-
 local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
     gems = tonumber(gems)
-
     if string.find(item, "Huge") and gems <= 100000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif item == "X-Large Christmas Present" and gems <= 50000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif string.find(item, "Titanic") and gems <= 50000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif string.find(item, "Exclusive") and gems <= 50000 then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username)
     elseif gems <= 25 then
@@ -102,7 +67,6 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
         processListingInfo(uid, gems, item, version, shiny, amount, username)
     end
 end
-
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
     local playerID = message['PlayerID']
     if type(message) == "table" then
@@ -112,10 +76,8 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
                 local uid = key
                 local gems = value["DiamondCost"]
                 local itemdata = value["ItemData"]
-
                 if itemdata then
                     local data = itemdata["data"]
-
                     if data then
                         local item = data["id"]
                         local version = data["pt"]
@@ -128,5 +90,3 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
         end
     end
 end)
-
-print("hello")
