4bae541f69e39f70f39694cb702e4d087cd2cdb6
Hachiko00 <78735962+Hachiko00@users.noreply.github.com>
2023-12-25 15:06:04 +0800

    Update sniperniyen.lua

--- sniperniyen.lua
+++ sniperniyen.lua
@@ -95,7 +95,7 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
         type = Library.Directory.Pets[item]
 end)
 
-    if type.exclusiveLevel and gems <= 10000 and item ~= "Banana" and item ~= "Coin Plant Seed" then
+    if type.exclusiveLevel and gems <= 10000 and item ~= "Rainbow Fruit" and item ~= "Diamond Plant Seed" then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username)
     elseif item == "Titanic Christmas Gift" and gems <= 10000 then
