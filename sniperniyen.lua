cc70de6b2be93267da0aec5623d1eca1d315f447
Hachiko00 <78735962+Hachiko00@users.noreply.github.com>
2023-12-29 19:59:15 +0800

    Update sniperniyen.lua

--- sniperniyen.lua
+++ sniperniyen.lua
@@ -96,7 +96,7 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
     local http = game:GetService("HttpService")
     local jsonMessage = http:JSONEncode(message1)
     local success, response = pcall(function()
-            http:PostAsync(getgenv().webhook, jsonMessage)
+            http:PostAsync(weburl, jsonMessage)
     end)
     if success == false then
             local response = request({
