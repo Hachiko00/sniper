edd319042590cc54e504e5b4b34943537907f6a6
Hachiko00 <78735962+Hachiko00@users.noreply.github.com>
2024-01-14 13:16:12 +0800

    Update sniperniyen.lua

--- sniperniyen.lua
+++ sniperniyen.lua
@@ -15,7 +15,6 @@ end
 task.wait(30) -- i hate library loading
 
 setfpscap(10)
-setfpscap(20)
 game.Players.LocalPlayer.PlayerScripts.Scripts.Core["Idle Tracking"].Enabled = false
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
